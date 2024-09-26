9b9b739db6af0f610906b74a4ab6cdb1e4a8d2b8
rpaul2707 <154616409+rpaul2707@users.noreply.github.com>
2024-09-26 19:51:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from flask import Flask, render_template, url_for
+from flask import request as req
+
+
+app = Flask(__name__)
+@app.route("/", methods=["GET","POST"])
+def Index():
+    return render_template("index.html")
+
+@app.route("/Summarize", methods=["GET", "POST"])
+def Summarize():
+    if req.method=="POST":
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": f"Bearer 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"}
+        data=req.form["data"]
+        minL=20
+        maxL=int(req.form["MaxL"])
+        def query(payload):
+
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+        
+        output = query({
+	        "inputs": data,
+            "parameters":{"min_length":minL, "max_length":maxL},
+        })
+        return render_template("index.html", result=output[0]['summary_text'])
+    else:
+        return render_template("index.html")
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
